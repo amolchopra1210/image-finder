56af54ef0cec0a87823426466130f5533a44e17f
Amol Chopra <achopra@wundermancommerce.com>
2019-05-02 15:00:17 +0530

    Updates

---
+++ precache-manifest.cae42b1fcef6dd4df1357f57499b4cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee9783b6dd652cc845f294260708ccc",
+    "url": "/image-finder/index.html"
+  },
+  {
+    "revision": "38acb0a98f97a9e05053",
+    "url": "/image-finder/static/css/main.af477205.chunk.css"
+  },
+  {
+    "revision": "d1d6ce1428a57fcb49cc",
+    "url": "/image-finder/static/js/2.1264a7ee.chunk.js"
+  },
+  {
+    "revision": "38acb0a98f97a9e05053",
+    "url": "/image-finder/static/js/main.e16826f2.chunk.js"
+  },
+  {
+    "revision": "f192cf7c5a241cf129dd",
+    "url": "/image-finder/static/js/runtime~main.26a19609.js"
+  }
+]);
